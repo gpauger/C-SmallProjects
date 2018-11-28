5908c86797977c770bb1927240b32fea22259659
gpauger <44099174+gpauger@users.noreply.github.com>
2018-11-28 12:14:25 -0800

    Added Acme files for drill
    Added the whole folder.  Not sure if that was necessary or if it would work with just .cs file.

---
+++ Acme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Acme
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string s = "Welcome to Acme Accounting System";
+            string t = "Remember, we are ''Accounting'' on you!";
+            Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+            Console.WriteLine(s);
+            Console.SetCursorPosition((Console.WindowWidth - t.Length) / 2, Console.CursorTop);
+            Console.WriteLine(t);
+            Console.ReadLine();
+        }
+    }
+}
