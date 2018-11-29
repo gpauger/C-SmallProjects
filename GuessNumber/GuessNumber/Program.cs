8225b6928c33b646a30dcda4bdd3123496f9d8b6
gpauger <44099174+gpauger@users.noreply.github.com>
2018-11-29 12:33:26 -0800

    guess number initial commit.

---
+++ GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+                Console.WriteLine("Let's play a game. Guess a number between 0-100.");
+                int rand = (new Random()).Next(0, 100);
+                int guess = Convert.ToInt32(Console.ReadLine());
+                do
+                {
+                    while (guess < rand)
+                    {
+                        Console.WriteLine("Guess higher!");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    }
+                    while (guess > rand)
+                    {
+                        Console.WriteLine("Guess lower!");
+                        guess = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                while (guess != rand);
+            Console.WriteLine("You guessed it!!!");
+            Console.ReadLine();
+        }
+    }
+}
