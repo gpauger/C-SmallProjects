f121d0e5e6035e8f629e70244d279ee557b2398e
gpauger <44099174+gpauger@users.noreply.github.com>
2018-11-29 11:14:57 -0800

    Initial commit for packageApp drill

---
+++ PackageApp/PackageApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PackageApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express.  Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            decimal weight = Convert.ToInt32(Console.ReadLine());
+            string error ="Package is too big to be shipped via Package Express. Have a nice day!";
+            if (weight > 50)
+            {
+                Console.WriteLine(error);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package height:");
+                decimal height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package width:");
+                decimal width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                decimal length = Convert.ToInt32(Console.ReadLine());
+                decimal volume = height + length + width;
+                decimal cost = volume * weight / 100;
+                if (50 >= volume)
+                {
+                    Console.WriteLine("Your estimated total for shipping will be $" + cost + ".  Thank you.");
+                }
+                else { 
+                Console.WriteLine(error);
+                }
+                    
+            }
+            Console.ReadLine();      
+            }
+            
+        }
+    }
+
