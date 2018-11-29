3fab2fe2d443469822669dd305985cebbba4877e
gpauger <44099174+gpauger@users.noreply.github.com>
2018-11-28 17:19:59 -0800

    Initial commit for qualify driver drill.

---
+++ QualifyDriver/QualifyDriver/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace QualifyDriver
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? true or false");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tix = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = (age > 15 && !dui && tix <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+           
+        }
+    }
+}
