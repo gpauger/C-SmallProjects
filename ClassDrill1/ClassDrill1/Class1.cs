54c0a91aa80225a5da0689c5aed8b78bcfffd2e8
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-05 14:55:50 -0800

    Initial upload

---
+++ ClassDrill1/ClassDrill1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill1
+{
+    public class Class1
+    {
+        public double Result1 { get; set; }
+        public double Result2 { get; set; }
+        public double Result3 { get; set; }
+    }
+}
---
+++ ClassDrill1/ClassDrill1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Class1 example = new Class1();
+            example.Result1 = input + input;
+            example.Result2 = input * input;
+            example.Result3 = Math.Pow(input,input);
+            Console.WriteLine(input + " added to itself = " + example.Result1);
+            Console.WriteLine(input + " multiplied by itself = " + example.Result2);
+            Console.WriteLine(input + " to the power of itself = " + example.Result3);
+            Console.ReadLine();
+        }
+    }
+}
