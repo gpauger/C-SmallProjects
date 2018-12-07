55cfd36fced27b4eefb44790374980956919c65b
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-07 14:29:15 -0800

    initial upload

---
+++ ClassesDrill5/ClassesDrill5/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDrill5
+{
+    public class Class1 
+    {//Creating method 1
+        public void Divided(int x)
+        {
+            int output = x / 2;
+          
+            Console.WriteLine("The number (int) you entered divided by 2 = " + output);
+        }
+        //creating method 2 with output parameter
+        public void ThankYou(out string ret)
+        {
+            ret = "Thank you!";
+            Console.WriteLine(ret);
+          
+        }
+        //Creating method 3 overloaded method 1
+        public void Divided(double y)
+        {
+            double output = y / 2;
+            Console.WriteLine("The number (double) you entered divided by 2 = " + output);
+        }
+        
+    }
+    //creating a static class
+    public static class MyMsg
+    {
+        public static void Log(string msg)
+        { Console.WriteLine("Goodbye!"); }
+        
+    }
+}
---
+++ ClassesDrill5/ClassesDrill5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDrill5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiates the class
+            Class1 n = new Class1();
+            Console.WriteLine("Please enter a number: ");
+           
+             var x = Convert.ToInt16(Console.ReadLine());
+            double y = x%2;
+            if (y == 1) //checks even or odd.  If input is even it continues as int else it converts to double
+            {
+                var z = Convert.ToDouble(x);  //The third method requires input as double
+                n.Divided(z); //calls 3rd (overload) method.  I realize changing the input variable defeats the purpose of an overload but I had to to make this work.
+            }
+            else { n.Divided(x); //calls first method
+            }
+            
+            string message;
+           
+            //calls the second method
+            n.ThankYou(out message);
+            //calls static message
+            MyMsg.Log("anystringwilldo?"); //wow, this feels clumsy and pointless but I guess it works.
+            
+
+       
+
+            Console.ReadLine();
+
+        }
+    }
+}
