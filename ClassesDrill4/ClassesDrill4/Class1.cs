cc925fac63f9f14c8994d5927f94e6e30a420ec3
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-07 12:03:36 -0800

    initial upload

---
+++ ClassesDrill4/ClassesDrill4/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDrill4
+{
+    public class Class1
+    {
+        //creates method used in Program.cs
+        public void Combine(double num1, string num2)
+        {
+            bool isNumeric = double.TryParse(num2, out double num2b);
+            if (isNumeric == true)
+            {
+                double num3 = num1 + num2b;
+                Console.WriteLine("Your numbers added together equal: " + num3);
+            }
+            else
+            {
+                double num3 = num1 + num1;
+                Console.WriteLine("Your number added to itself is: " + num3);
+            }
+            
+        }
+    }
+}
---
+++ ClassesDrill4/ClassesDrill4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDrill4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Give me another number (optional) or just press enter:");
+            string num2 = Console.ReadLine();
+            //instantiates the class
+            Class1 n = new Class1();
+            //Calls the method
+            n.Combine(num1, num2);
+            Console.ReadLine();
+
+        }
+    }
+}
