e3f888aa36db5f52a1803ce5e15aa9a8d96274b6
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-07 11:35:34 -0800

    intial upload

---
+++ ClassesDrill3/ClassesDrill3/Calculate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDrill3
+{
+    public class Calculate
+    {
+        int rounded; //saved vairable for third method
+        //creates first method used in Program.cs
+        public void Double(int num1)
+        {
+            int num2 = num1 * 2;
+            Console.WriteLine(num2);
+        }
+
+        //creates second method used in Program.cs
+        public void Double(decimal num1)  //name still Double even though this method tripples to satisfy drill requirement
+        {
+            decimal num2 = num1 * 3;
+            Console.WriteLine(Convert.ToInt16(num2) + " I rounded.  Sue me.");
+            rounded = Convert.ToInt16(num2);
+        }
+
+        //creates third method used in Program.cs
+        public void Double(string num1)  //name still Double even though this method tripples to satisfy drill requirement
+        {
+            try
+            {
+                decimal converted1= Convert.ToDecimal(num1); //tihs is silly but the expected input is a string
+                int converted2 = Convert.ToInt16(converted1); //that converts to a decimal more readily than an int.
+            int num2 = rounded - converted2;
+                Console.WriteLine("I was only off by : " + Convert.ToInt16(num2) + " What?  I like to round things.");
+            }
+            catch (FormatException)
+            { Console.WriteLine("Sorry, I cant read what you wrote.  Goodbye."); }
+        }
+    }
+
+}
---
+++ ClassesDrill3/ClassesDrill3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number and I will double it:");
+            int amount = Convert.ToInt16(Console.ReadLine());
+            //instantiate the calss
+            Calculate n = new Calculate();
+            //call the method
+            n.Double(amount);
+
+            Console.WriteLine("Too easy, give me a decimal and I will tripple it:");
+            decimal amount2 = Convert.ToDecimal(Console.ReadLine());
+            n.Double(amount2);
+
+            Console.WriteLine("What should I have said? :");
+            string amount3 = Console.ReadLine();
+            n.Double(amount3);
+            Console.ReadLine();
+        }
+    }
+}
