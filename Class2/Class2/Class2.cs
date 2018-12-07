5c7ef6bb92f7072846f914564c3470b78eafa095
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-06 20:50:46 -0800

    Initial Commit

---
+++ Class2/Class2/Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    public class Class2
+    {
+        //creates method used in Program.cs
+        public void Combine(double num1, double num2)
+        {
+            double num3 = num1 + num2;
+            Console.WriteLine(num3);
+        }
+    }
+}
---
+++ Class2/Class2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            
+            //instantiates class2
+            Class2 comp = new Class2();
+            //calls the method from class2
+            
+            comp.Combine(x, y);
+            comp.Combine(num1: x, num2: y);
+            Console.ReadLine();
+
+
+        }
+    }
+}
