642f424b88f31af76d317d79f8e74c44c525306f
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-11 11:00:07 -0800

    Initial upload

---
+++ InheritanceDril1/InheritanceDril1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDril1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initializes the class: Employee
+            Employee sample = new Employee()
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+            //Calls the method SayName from class: Employee
+            sample.SayName();
+            Console.ReadLine();
+        }
+    }
+}
