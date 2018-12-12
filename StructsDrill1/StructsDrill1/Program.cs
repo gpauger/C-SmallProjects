8603a843f3934c31658a77b742dbbc98cd215c49
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-12 13:45:32 -0800

    Initial upload

---
+++ StructsDrill1/StructsDrill1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsDrill1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //instantiates a new number
+            Number x = new Number()
+            { Amount = 0.75M };  //sets number's value property
+            Console.WriteLine(x.Amount);
+            Console.ReadLine();
+        }
+    }
+}
