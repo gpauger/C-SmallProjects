07c65a43ba674a43ee23f8efc90937afec3313bc
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-12 11:37:31 -0800

    initial upload

---
+++ GenericsDrill1/GenericsDrill1/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDrill1
+
+{
+    public class Employee<T>  //class takes generic type parameter
+    {
+        
+        //creates a generic property- list of things
+        public List<T> Things { get; set; }
+
+        //Creates a function to loop through list "Things" and print each item
+        public void listThings()
+        {
+            int i = 0;
+            while (i < Things.Count)
+            {
+                Console.WriteLine(Things[i]);
+                i++;
+            }
+        }
+
+
+        //public static Boolean operator ==(Employee emp1, Employee emp2)
+        //{
+
+        //    if (emp1.Id == emp2.Id)
+        //    { return true; }
+        //    else { return false; }
+
+
+        //}
+        //public static Boolean operator !=(Employee emp1, Employee emp2)
+        //{
+
+        //    if (emp1.Id != emp2.Id)
+        //    { return true; }
+        //    else { return false; }
+
+
+        //}
+    }
+}
+
---
+++ GenericsDrill1/GenericsDrill1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDrill1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiates the class: Employee
+            Employee<string> sample = new Employee<string>();
+            sample.Things = new List<string>()
+            {
+                 "computer", "charger", "notebook", "pen"    
+            };
+            Employee<int> sample2 = new Employee<int>();
+            sample2.Things = new List<int>()
+            {
+                8, 6, 7, 5, 3, 0, 9
+            };
+
+
+            
+            //Calls the method listThings from class: Employee
+            
+            sample.listThings();
+            sample2.listThings();
+            Console.ReadLine();
+        }
+    }
+}
