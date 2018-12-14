fa1dbb4d26ae8076e77e71ffbbbe7692f378b07f
gpauger <44099174+gpauger@users.noreply.github.com>
2018-12-14 12:32:36 -0800

    initial upload

---
+++ InOutDrill1/InOutDrill1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace InOutDrill1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//this would work just as well for text, which seems fine for this drill.  Could change num to int instead of string but would need a couple of casts.
+            Console.WriteLine("Please enter a number: ");
+            string num = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\gpaug\Dropbox\Tech Academy\C-SmallProjects\InOutDrill1\log.txt", true))
+            {
+                file.WriteLine(num);
+            };
+            string text = File.ReadAllText(@"C:\Users\gpaug\Dropbox\Tech Academy\C-SmallProjects\InOutDrill1\log.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
