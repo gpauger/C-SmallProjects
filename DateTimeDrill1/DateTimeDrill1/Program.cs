cc37e5eecfb9da28e093361c4de3d7923a1a1968
gpauger <44099174+gpauger@users.noreply.github.com>
2019-01-02 20:33:24 -0800

    initial commit

---
+++ DateTimeDrill1/DateTimeDrill1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine("The current time is: " + DateTime.Now);
+            Console.WriteLine("Please enter a number: ");
+            Int16 x = Convert.ToInt16(Console.ReadLine());
+            DateTime future = dateTime.AddHours(x);
+       
+            Console.WriteLine("In " + x + " hours, it will be: " + future);
+            Console.ReadLine();
+
+        }
+    }
+}
