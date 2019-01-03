a617f2dc0b553e0227ab278c385e7ac1e59035c9
gpauger <44099174+gpauger@users.noreply.github.com>
2019-01-03 13:47:15 -0800

    Initial commit
    not working the way I want it to

---
+++ ConstDrill1/ConstDrill1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstDrill1
+{
+    public class User
+    {
+        public string name { get; set; }
+        public int ID { get; set; }
+        public int trials { get; set; }
+
+        public User(string name) : this(name, 8675309, 3)
+        { }
+        public User(string name, int ID, int trials)
+        { }
+       
+                
+    }
+
+    
+        
+   
+    
+}
---
+++ ConstDrill1/ConstDrill1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstDrill1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //creates a constant string that can be reused by calling the variable 'welcome'
+            const string welcome = "Hello and welcome!  Please enter your name:";
+            Console.WriteLine(welcome);
+            //creates generic varialbe which C# should recognize as a string
+            var name = Console.ReadLine();
+            //can't figure out why this wont instantiate
+            User user1 = new User(name); 
+          
+            Console.WriteLine("Thanks " + user1.name);
+            Console.WriteLine("Your ID is: " + user1.ID );
+            Console.WriteLine("You have " + user1.trials + " free trials remaining.");
+            Console.ReadLine();
+        }
+         
+    }
+}
